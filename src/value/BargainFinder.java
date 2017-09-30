196f1be3a05cbaa1c2f5c27ffacc422af64bd1bb
Nathan Hardy <nhardy@live.com.au>
2017-10-01 11:21:59 +1300

    Create a HashMap for wantedItems

--- src/value/BargainFinder.java
+++ src/value/BargainFinder.java
@@ -1,14 +1,17 @@
 package value;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
 
 /**
  * The class for bargain finders.
- * 
+ *
  * @author MichaelAlbert
  */
 public class BargainFinder {
-    
+
     private SiteInfo site;
     private CustomerInfo customer;
     private int budget;
@@ -18,12 +21,21 @@ public class BargainFinder {
         this.customer = customer;
         this.budget = budget;
     }
-    
+
     public ArrayList<String> shoppingList() {
-    	//Implement me :)
-    	
-    	
+        HashMap<String, Integer> wantedItems = new HashMap<String, Integer>();
+        for (String item : customer.getItems()) {
+            wantedItems.put(item, customer.getValue(item));
+        }
+
+        HashSet<HashSet<String>> seen = new HashSet<HashSet<String>>();
+
+        // ArrayList<String> itemsLeft = new ArrayList<String>();
+        // itemsLeft.addAll(wantedItems.keySet());
+
     	return null;
     }
+
+
 
 }
