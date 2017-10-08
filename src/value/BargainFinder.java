d78c95e422584d2bcaf1a1bcc988f2d603a9b456
Nathan Hardy <nhardy@live.com.au>
2017-10-08 14:09:24 +1300

    Hopefully speed things up

--- src/value/BargainFinder.java
+++ src/value/BargainFinder.java
@@ -5,8 +5,7 @@
 package value;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 
 /**
  * The class for bargain finders.
@@ -26,44 +25,24 @@ public class BargainFinder {
     }
 
     public ArrayList<String> shoppingList() {
-        HashMap<String, Integer> wantedItems = new HashMap<String, Integer>();
-        for (String item : customer.getItems()) {
-            wantedItems.put(item, customer.getValue(item));
-        }
-
-        HashSet<HashSet<String>> seen = new HashSet<HashSet<String>>();
-
-        ArrayList<String> bestItems = new ArrayList<String>();
-        bestItems.addAll(findBestItems(new Pair<HashSet<String>, Integer>(new HashSet<String>(), Integer.MIN_VALUE), customer.getItems(), seen).getKey());
-
-    	return bestItems;
+        return findBestItems(customer.getItems(), budget, new Pair<ArrayList<String>, Integer>(new ArrayList<>(), 0)).getKey();
     }
 
-    private Pair<HashSet<String>, Integer> findBestItems(Pair<HashSet<String>, Integer> current, ArrayList<String> itemsLeft, HashSet<HashSet<String>> seen) {
-        Pair<HashSet<String>, Integer> max = current;
-        for (String item : itemsLeft) {
-            HashSet<String> next = new HashSet<String>();
-            next.addAll(current.getKey());
-            next.add(item);
-            if (seen.contains(next)) continue;
+    private Pair<ArrayList<String>, Integer> findBestItems(List<String> items, int budget, Pair<ArrayList<String>, Integer> partial) {
+        Pair<ArrayList<String>, Integer> max = partial;
+        for (int i = 0; i < items.size(); i++) {
+            String item = items.get(i);
+            ArrayList<String> tmp = new ArrayList<>();
+            tmp.addAll(partial.getKey());
+            tmp.add(item);
 
-            ArrayList<String> nextArray = new ArrayList<String>();
-            nextArray.addAll(next);
-            if (site.getCost(nextArray) > budget) continue;
+            if (site.getCost(tmp) > budget) return partial;
 
-            int gain = 0;
-            for (String i : nextArray) {
-                gain += customer.getValue(i);
-            }
-
-            ArrayList<String> newItemsLeft = new ArrayList<String>();
-            for (String itemLeft : itemsLeft) {
-                if (!itemLeft.equals(item)) newItemsLeft.add(itemLeft);
-            }
-
-            Pair<HashSet<String>, Integer> deeper = findBestItems(new Pair<HashSet<String>, Integer>(next, gain), newItemsLeft, seen);
-
-            max = deeper.getValue() > max.getValue() ? deeper : max;
+            Pair<ArrayList<String>, Integer> deeper = findBestItems(
+                items.subList(i + 1, items.size()), budget,
+                new Pair<ArrayList<String>, Integer>(tmp, partial.getValue() + customer.getValue(item))
+            );
+            if (deeper.getValue() > max.getValue()) max = deeper;
         }
 
         return max;
